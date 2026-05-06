743e02f2892293c2a75488418e4f8062654142da
zekai.lin <zekai.lin@gzidc.com>
2026-05-06 17:16:41 +0800

    chore: release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="symbiopulse",
-    version="0.1.12",
+    version="0.1.0",
     author="SymbioPath Contributors",
     author_email="maintainers@symbiopulse.io",
     description="Code context manager with evolutionary instinct for AI agents",
